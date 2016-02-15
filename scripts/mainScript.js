a866b398a74b0ab7e78534611ac7b8398d0253f7
tamunaKap <tamuna.kapanadzee@gmail.com>
2016-02-15 19:08:46 +0400

    second section strater

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -12,6 +12,20 @@ $(function () {
         $(".centerBlock iframe").attr("src", "");
     });
     $('[data-toggle="tooltip"]').tooltip();
+
+    //$('.feast img').mouseenter(function () {
+    //    var src = this.src;
+    //    if (src.indexOf("supra") == -1) {
+    //        $(this).attr("src", src.replace(".png", "bubble.png"));
+    //    }
+    //}).mouseleave(function () {
+    //    var src = this.src;
+    //    if (src.indexOf("supra") == -1) {
+    //        $(this).attr("src", src.replace("bubble.png", ".png"));
+    //    }
+    //});
+
+
     //var controller = new ScrollMagic.Controller();
     //var tween = TweenMax.from("#article", 1, { css: { scaleX: 0.5, scaleY: 0.5 }, ease: Linear.easeNone });
 
@@ -21,50 +35,50 @@ $(function () {
     //				.addTo(controller);
     //scene.removeIndicators();
 
-    var flightpath = {
-        entry: {
-            curviness: 1.25,
-            autoRotate: true,
-            values: [
-                    { x: 10, y: 10 } 
-            ]
-        },
-        looping: {
-            curviness: 1.25,
-            autoRotate: true,
-            values: [
-                    { x: 10, y: 60 },
-                    { x: 20, y: -60 },
-                    { x: 30, y: -100 },
-                    { x: 80, y: 20 },
-                    { x: 100, y: 160 },
-                    { x: 200, y: 120 },
-                    { x: 620, y: 550 }
-            ]
-        },
-        leave: {
-            curviness: 1.25,
-            autoRotate: true,
-            values: [
-                    { x: 660, y: 20 },
-                    { x: 800, y: 130 },
-                    { x: $(window).width() + 300, y: -100 },
-            ]
-        }
-    };
-    // init controller
-    var controller = new ScrollMagic.Controller();
+    //var flightpath = {
+    //    entry: {
+    //        curviness: 1.25,
+    //        autoRotate: true,
+    //        values: [
+    //                { x: 10, y: 10 }
+    //        ]
+    //    },
+    //    looping: {
+    //        curviness: 1.25,
+    //        autoRotate: true,
+    //        values: [
+    //                { x: 10, y: 60 },
+    //                { x: 20, y: -60 },
+    //                { x: 30, y: -100 },
+    //                { x: 80, y: 20 },
+    //                { x: 100, y: 160 },
+    //                { x: 200, y: 120 },
+    //                { x: 620, y: 550 }
+    //        ]
+    //    },
+    //    leave: {
+    //        curviness: 1.25,
+    //        autoRotate: true,
+    //        values: [
+    //                { x: 660, y: 20 },
+    //                { x: 800, y: 130 },
+    //                { x: $(window).width() + 300, y: -100 },
+    //        ]
+    //    }
+    //};
+    //// init controller
+    //var controller = new ScrollMagic.Controller();
 
-    // create tween
-    var tween = new TimelineMax()
-        .add(TweenMax.to($("#plane"), 1.2, { css: { bezier: flightpath.entry }, ease: Power1.easeInOut }))
-        .add(TweenMax.to($("#plane"), 2, { css: { bezier: flightpath.looping }, ease: Power1.easeInOut }))
-        .add(TweenMax.to($("#plane"), 1, { css: { bezier: flightpath.leave }, ease: Power1.easeInOut }));
+    //// create tween
+    //var tween = new TimelineMax()
+    //    .add(TweenMax.to($("#plane"), 1.2, { css: { bezier: flightpath.entry }, ease: Power1.easeInOut }))
+    //    .add(TweenMax.to($("#plane"), 2, { css: { bezier: flightpath.looping }, ease: Power1.easeInOut }))
+    //    .add(TweenMax.to($("#plane"), 1, { css: { bezier: flightpath.leave }, ease: Power1.easeInOut }));
 
-    // build scene
-    var scene = new ScrollMagic.Scene({ triggerElement: "#target", duration: 500, offset: 0 }) 
-                    .setTween(tween)
-                    .addIndicators() // add indicators (requires plugin)
-                    .addTo(controller);
-    scene.removeIndicators();
+    //// build scene
+    //var scene = new ScrollMagic.Scene({ triggerElement: "#target", duration: 500, offset: 0 })
+    //                .setTween(tween)
+    //                .addIndicators() // add indicators (requires plugin)
+    //                .addTo(controller);
+    //scene.removeIndicators();
 })
