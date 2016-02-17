ba5ebd6035e7e86e0759aa5f0d70be2f33aab9a2
tamunaKap <tamuna.kapanadzee@gmail.com>
2016-02-17 18:42:59 +0400

    second page concept change

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,4 +1,4 @@
-$(function () { 
+$(function () {
     $(".centerBlock").hide();
     $(".girls figure").click(function () {
         var youtubeUrl = $(this).data("youtube");
@@ -23,11 +23,24 @@ $(function () {
             $(this).siblings("img").hide();
         }
     }).click(function () {
-
+        $(".feast").animate({
+            "left": "350px"
+        }, 1000);
+        $(".featsContent").animate({
+            "right": "0"
+        }, 1000);
+    });
+    $(".featsContent .glyphicon-remove").click(function () {
+        $(".feast").animate({
+            "left": "50%",
+            "margin-left": "-350px"
+        }, 1000);
+        $(".featsContent").animate({
+            "right": "-100%"
+        }, 1000);
     });
 
-    $('#fullpage').fullpage({
-        //Navigation
+    $('#fullpage').fullpage({ 
         menu: '#menu',
         lockAnchors: false,
         anchors: ['home', 'feast'],
@@ -37,106 +50,5 @@ $(function () {
         showActiveTooltip: false,
         slidesNavigation: true, //false
         slidesNavPosition: 'bottom',
-
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-        //Design
-        controlArrows: false,
-        verticalCentered: true,
-        resize: false,
-        sectionsColor: ['#ccc', '#fff'],
-        paddingTop: '3em',
-        paddingBottom: '10px',
-        fixedElements: '#header, .footer',
-        responsiveWidth: 0,
-        responsiveHeight: 0,
-
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index) { },
-        afterRender: function () { },
-        afterResize: function () { },
-        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
-        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
-    });
-    //var controller = new ScrollMagic.Controller();
-    //var tween = TweenMax.from("#article", 1, { css: { scaleX: 0.5, scaleY: 0.5 }, ease: Linear.easeNone });
-
-    //var scene = new ScrollMagic.Scene({ triggerElement: "#article", duration: 300 })
-    //				.setTween(tween)
-    //				.addIndicators() // add indicators (requires plugin)
-    //				.addTo(controller);
-    //scene.removeIndicators();
-
-    //var flightpath = {
-    //    entry: {
-    //        curviness: 1.25,
-    //        autoRotate: true,
-    //        values: [
-    //                { x: 10, y: 10 }
-    //        ]
-    //    },
-    //    looping: {
-    //        curviness: 1.25,
-    //        autoRotate: true,
-    //        values: [
-    //                { x: 10, y: 60 },
-    //                { x: 20, y: -60 },
-    //                { x: 30, y: -100 },
-    //                { x: 80, y: 20 },
-    //                { x: 100, y: 160 },
-    //                { x: 200, y: 120 },
-    //                { x: 620, y: 550 }
-    //        ]
-    //    },
-    //    leave: {
-    //        curviness: 1.25,
-    //        autoRotate: true,
-    //        values: [
-    //                { x: 660, y: 20 },
-    //                { x: 800, y: 130 },
-    //                { x: $(window).width() + 300, y: -100 },
-    //        ]
-    //    }
-    //};
-    //// init controller
-    //var controller = new ScrollMagic.Controller();
-
-    //// create tween
-    //var tween = new TimelineMax()
-    //    .add(TweenMax.to($("#plane"), 1.2, { css: { bezier: flightpath.entry }, ease: Power1.easeInOut }))
-    //    .add(TweenMax.to($("#plane"), 2, { css: { bezier: flightpath.looping }, ease: Power1.easeInOut }))
-    //    .add(TweenMax.to($("#plane"), 1, { css: { bezier: flightpath.leave }, ease: Power1.easeInOut }));
-
-    //// build scene
-    //var scene = new ScrollMagic.Scene({ triggerElement: "#target", duration: 500, offset: 0 })
-    //                .setTween(tween)
-    //                .addIndicators() // add indicators (requires plugin)
-    //                .addTo(controller);
-    //scene.removeIndicators();
+    }); 
 })
